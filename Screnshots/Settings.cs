868705a431dbb28cb827f0f1b74e99ccdef938c6
Александр Ручиц <Sasha.ruchits.06@gmail.com>
2022-08-31 15:46:47 +0300

    <Обязательно>

--- Screnshots/Settings.cs
+++ Screnshots/Settings.cs
@@ -9,84 +9,67 @@ namespace Screnshots
 {
     public static class Settings
     {
-        private static Key _FirstKeyShow=(Key)GlobalSettings.Default.FirstKeyShow;
-        private static Key _SecondKeyShow=(Key)GlobalSettings.Default.SecondKeyShow;
-        private static Key _FirstKeyHide = (Key)GlobalSettings.Default.FirstKeyHide;
-        private static Key _SecondKeyHide= (Key)GlobalSettings.Default.SecondKeyHide;
-        private static Key _CreateScreenshotFirstKey= (Key)GlobalSettings.Default.CreateScreenshotFirstKey;
-        private static Key _CreateScreenshotSecondKey= (Key)GlobalSettings.Default.CreateScreenshotSecondKey;
-        private static long _NumberSave = GlobalSettings.Default.NumberSave;
-        private static String _PathSave = GlobalSettings.Default.PathSave;
-
         public static Key FirstKeyShow
         {            
-            get { return _FirstKeyShow; }
+            get { return (Key)GlobalSettings.Default.FirstKeyShow; }
             set
             {
-                _FirstKeyShow = value;
                 GlobalSettings.Default.FirstKeyShow=(int)value;
             }
         }
         public static Key SecondKeyShow
         {
-            get { return _SecondKeyShow; }
+            get { return (Key)GlobalSettings.Default.SecondKeyShow; }
             set
             {
-                _SecondKeyShow = value;
                 GlobalSettings.Default.SecondKeyShow=(int)value;
             }
         }
         public static Key FirstKeyHide
         {
-            get { return _FirstKeyHide; }
+            get { return (Key)GlobalSettings.Default.FirstKeyHide; }
             set
             {
-                _FirstKeyHide = value;
                 GlobalSettings.Default.FirstKeyHide=(int)value;
             }
         }
         public static Key SecondKeyHide
         {
-            get { return _SecondKeyHide; }
+            get { return (Key)GlobalSettings.Default.SecondKeyHide; }
             set
             {
-                _SecondKeyHide = value;
                 GlobalSettings.Default.SecondKeyHide = (int)value;
             }
         }
         public static Key CreateScreenshotFirstKey
         {
-            get { return _CreateScreenshotFirstKey; }
+            get { return (Key)GlobalSettings.Default.CreateScreenshotFirstKey; }
             set
             {
-                _CreateScreenshotFirstKey = value;
                 GlobalSettings.Default.CreateScreenshotFirstKey = (int)value;
             }
         }
         public static Key CreateScreenshotSecondKey
         {
-            get{ return _CreateScreenshotSecondKey; }
+            get{ return (Key)GlobalSettings.Default.CreateScreenshotSecondKey; }
             set
             {
-                _CreateScreenshotSecondKey = value;
                 GlobalSettings.Default.CreateScreenshotSecondKey = (int)value;
             }
         }
         public static long NumberSave
         {
-            get { return _NumberSave; }
+            get { return GlobalSettings.Default.NumberSave; }
             set
             {
-                _NumberSave = value;
                 GlobalSettings.Default.NumberSave = value;
             }
         }
         public static String PathSave
         {
-            get { return _PathSave; }
+            get { return GlobalSettings.Default.PathSave; }
             set
             {
-                _PathSave = value;
                 GlobalSettings.Default.PathSave = value;
             }
         }
